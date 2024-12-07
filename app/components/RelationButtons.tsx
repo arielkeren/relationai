5a2a04fb64cf93996b4bbc5a3044c25004e220c1
shmil123 <arielkeren2007@gmail.com>
2024-12-07 18:08:15 +0200

    Change the icons of the buttons

--- app/components/RelationButtons.tsx
+++ app/components/RelationButtons.tsx
@@ -1,6 +1,5 @@
 import { FaDiceThree } from "react-icons/fa";
-import { IoSunny } from "react-icons/io5";
-import { IoMoon } from "react-icons/io5";
+import { PiEmptyBold } from "react-icons/pi";
 
 type Props = {
   toggleAllOn: () => void;
@@ -17,21 +16,21 @@ const RelationButtons: React.FC<Props> = ({
     <div className="flex gap-[4px]">
       <button
         onClick={toggleAllOff}
-        className="flex justify-center items-center gap-1 text-2xl bg-gray-800 text-white h-16 w-[100px] rounded drop-shadow-md uppercase transition-colors hover:bg-gray-700"
+        className="flex justify-center items-center gap-1 text-3xl bg-gray-800 text-white h-16 w-[100px] rounded drop-shadow-md transition-colors hover:bg-gray-700"
       >
-        <IoMoon />
+        <PiEmptyBold />
       </button>
       <button
         onClick={toggleRandom}
-        className="flex justify-center items-center gap-1 text-2xl bg-gray-800 text-white h-16 w-[132px] rounded drop-shadow-md uppercase transition-colors hover:bg-gray-700"
+        className="flex justify-center items-center gap-1 text-2xl bg-gray-800 text-white h-16 w-[132px] rounded drop-shadow-md transition-colors hover:bg-gray-700"
       >
         <FaDiceThree />
       </button>
       <button
         onClick={toggleAllOn}
-        className="flex justify-center items-center gap-1 text-3xl bg-gray-800 text-white h-16 w-[100px] rounded drop-shadow-md uppercase transition-colors hover:bg-gray-700"
+        className="flex justify-center items-center gap-1 text-3xl bg-gray-800 text-white h-16 w-[100px] rounded drop-shadow-md transition-colors hover:bg-gray-700"
       >
-        <IoSunny />
+        A²
       </button>
     </div>
   );
