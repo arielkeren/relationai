31cad8d64ba9edf88bd3a858ee564d08ec06e85c
shmil123 <arielkeren2007@gmail.com>
2024-12-09 21:20:47 +0200

    Add a relation cell component

--- app/components/Relation.tsx
+++ app/components/Relation.tsx
@@ -1,28 +1,26 @@
+import RelationCell from "./RelationCell";
+
 type Props = {
   relation: number[][];
   togglePair: (i: number, j: number) => void;
 };
 
-const Relation: React.FC<Props> = ({ relation, togglePair }) => {
-  return (
-    <div className="flex flex-col gap-1">
-      {relation.map((row, i) => (
-        <div key={i} className="flex gap-1">
-          {row.map((cell, j) => (
-            <button
-              onClick={() => togglePair(i, j)}
-              key={`${i}${j}`}
-              className={`h-[60.8px] w-[60.8px] rounded transition-colors duration-300 ${
-                cell
-                  ? "bg-gray-800 hover:bg-gray-700"
-                  : "bg-gray-300 hover:bg-gray-200"
-              }`}
-            ></button>
-          ))}
-        </div>
-      ))}
-    </div>
-  );
-};
+const Relation: React.FC<Props> = ({ relation, togglePair }) => (
+  <div className="flex flex-col gap-1">
+    {relation.map((row, i) => (
+      <div key={i} className="flex gap-1">
+        {row.map((cell, j) => (
+          <RelationCell
+            isOn={!!cell}
+            i={i}
+            j={j}
+            togglePair={togglePair}
+            key={`${i}${j}`}
+          />
+        ))}
+      </div>
+    ))}
+  </div>
+);
 
 export default Relation;
---
+++ app/components/RelationCell.tsx
@@ -0,0 +1,17 @@
+type Props = {
+  isOn: boolean;
+  i: number;
+  j: number;
+  togglePair: (i: number, j: number) => void;
+};
+
+const RelationCell: React.FC<Props> = ({ isOn, i, j, togglePair }) => (
+  <button
+    onClick={() => togglePair(i, j)}
+    className={`h-[60.8px] w-[60.8px] rounded transition-colors duration-300 ${
+      isOn ? "bg-gray-800 hover:bg-gray-700" : "bg-gray-300 hover:bg-gray-200"
+    }`}
+  ></button>
+);
+
+export default RelationCell;
