3e5bc073243da52342669b9d3a7c07538bf93622
shmil123 <arielkeren2007@gmail.com>
2024-12-09 21:48:07 +0200

    Fix font size of the identity relation button

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -15,13 +15,13 @@ const Button: React.FC<Props> = ({
 }) => (
   <button
     onClick={onClick}
-    className={`flex justify-center items-center gap-1 bg-gray-800 text-white h-[60.8px] rounded drop-shadow-md transition-colors ${
+    className={`flex justify-center items-center gap-1 text-3xl bg-gray-800 text-white h-[60.8px] rounded drop-shadow-md transition-colors ${
       isWide ? "w-[320px]" : "w-[60.8px]"
     } ${
       isGradient
         ? "hover:bg-gradient-to-br hover:from-pink-500 hover:to-blue-500 duration-300"
         : "hover:bg-gray-700"
-    } ${isMono ? "font-mono text-4xl" : "text-3xl"}`}
+    } ${isMono && "font-mono"}`}
   >
     {children}
   </button>
